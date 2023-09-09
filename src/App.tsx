346a4ef5bf38bbd696ae37ec9ae461fd77b88a01
Daniel <danielukoha101@gmail.com>
2023-09-09 14:44:29 +0100

    add utc day and time

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,34 @@
+import { useEffect, useState } from "react";
 import photo from "./assets/profile.jpeg";
 
+const DAYS = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
 function App() {
+  const [day, setDay] = useState("");
+  const [utcTime, setUtcTime] = useState("");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const day = DAYS[new Date().getUTCDay()];
+      const hours = String(new Date().getUTCHours()).padStart(2, "0");
+      const minutes = String(new Date().getUTCMinutes()).padStart(2, "0");
+      const seconds = String(new Date().getUTCSeconds()).padStart(2, "0");
+      setDay(day);
+      setUtcTime(`${hours}:${minutes}:${seconds}`);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
   return (
     <main className="p-4 flex flex-col gap-4 text-center">
       <section className="flex flex-col items-center py-2">
@@ -18,12 +46,12 @@ function App() {
       </section>
       <section className="flex flex-col items-center py-2">
         <div className="mb-4 text-lg">
-          <h4 className="text-sm">Day and time:</h4>
+          <h4 className="text-sm">Day and time (UTC):</h4>
           <p className="font-semibold" data-testid="currentDayOfTheWeek">
-            Day of the week
+            {day}
           </p>
           <p className="font-semibold" data-testid="currentUTCTime">
-            Time (UTC)
+            {utcTime}
           </p>
         </div>
         <p data-testid="myTrack">Track: Frontend</p>
